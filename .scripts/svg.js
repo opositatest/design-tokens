92b2a55a30d513195d4f349edc42f8030caad5df
Diego Lorenzo <dlorenzonespereira@gmail.com>
2022-12-15 12:10:40 +0100

    Add script to minify svgs automatically

---
+++ .scripts/svg.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+const path = require('path');
+const execSync = require('child_process').execSync;
+const { optimize } = require('svgo');
+const { parse, stringify } = require('svgson');
+
+const replaceHexColors = (svgElement) => {
+  if (svgElement.attributes) {
+    for (const [key, value] of Object.entries(svgElement.attributes)) {
+      const hexColorRegex = /#[0-9A-Fa-f]{6}/g;
+      if (hexColorRegex.test(value)) {
+        svgElement.attributes[key] = value.replace(
+          hexColorRegex,
+          'currentColor'
+        );
+      }
+    }
+  }
+
+  if (svgElement.children) {
+    svgElement.children.forEach((child) => {
+      replaceHexColors(child);
+    });
+  }
+};
+
+const modifiedFiles = execSync(
+  'git status --porcelain | cut -c 1-3 --complement'
+)
+  .toString()
+  .split('\n')
+  .filter((file) => file.endsWith('.svg'));
+
+modifiedFiles.forEach((file) => {
+  fs.readFile(file, (err, data) => {
+    if (err) throw err;
+
+    const result = optimize(data, {
+      path: file,
+      plugins: [
+        {
+          name: 'preset-default',
+          params: {
+            overrides: {
+              convertColors: false,
+              removeViewBox: false,
+            },
+          },
+        },
+        'convertStyleToAttrs',
+        'removeDimensions',
+        'removeScriptElement',
+        'removeStyleElement',
+      ],
+    });
+    parse(result.data, {
+      camelcase: true,
+    }).then((svgTree) => {
+      let className = path.basename(file).split('.svg').join('');
+      if (file.indexOf('categories') !== -1) {
+        className = `cat-${className}`;
+      }
+
+      svgTree.attributes = {
+        ...svgTree.attributes,
+        class: `Icon Icon-${className}`,
+      };
+
+      replaceHexColors(svgTree);
+
+      const updatedSvgContent = stringify(svgTree);
+      fs.writeFile(file, updatedSvgContent, (err) => {
+        if (err) throw err;
+      });
+    });
+  });
+});
